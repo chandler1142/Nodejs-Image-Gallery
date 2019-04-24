fd7c9f9660d80124b5fa91d29bfa3b8befc948ee
thzw1142 <thzw1142@163.com>
2019-04-24 10:39:14 +0800

    gensture direction

--- handmodel.js
+++ handmodel.js
@@ -9,7 +9,7 @@ const {createCanvas, loadImage} = require('canvas');
 var load = function() {
 	
 	const modelParams = {
-		flipHorizontal: true, // flip e.g for video  
+		flipHorizontal: false, // flip e.g for video  
 		maxNumBoxes: 1, // maximum number of boxes to detect
 		iouThreshold: 0.5, // ioU threshold for non-max suppression
 		scoreThreshold: 0.6, // confidence threshold for predictions.
